261a661b2330cc0777fae2679eb8581c6becbd1a
Fisayo Afolayan <afolayanfisayo@gmail.com>
2020-10-06 16:35:32 +0200

    Clean up

--- add_admin.php
+++ add_admin.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 require_once './config/config.php';
 require_once 'includes/auth_validate.php';
@@ -11,20 +12,20 @@ if ($_SESSION['admin_type'] !== 'super') {
 }
 
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') 
-{
-	$data_to_store = filter_input_array(INPUT_POST);
-    $db = getDbInstance();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $data = filter_input_array(INPUT_POST);
+    $dbInstance = getDbInstance();
     //Password should be md5 encrypted
-    $data_to_store['passwd'] = md5($data_to_store['passwd']);
-    $last_id = $db->insert ('admin_accounts', $data_to_store);
-    if($last_id)
-    {
-    	$_SESSION['success'] = "Admin user added successfully!";
-    	header('location: admin_users.php');
-    	exit();
-    }  
-    
+    $data['passwd'] = md5($data['passwd']);
+	
+    $storedDataId = $dbInstance->insert('admin_accounts', $data);
+	
+    if ($storedDataId) {
+        $_SESSION['success'] = "Admin user added successfully!";
+        header('location: admin_users.php');
+        exit();
+    }
+
 }
 
 $edit = false;
@@ -32,17 +33,17 @@ $edit = false;
 
 require_once 'includes/header.php';
 ?>
-<div id="page-wrapper">
-	<div class="row">
-		<div class="col-lg-12">
-			<h2 class="page-header">Add User</h2>
-		</div>
-	</div>
-	<!-- Success message -->
-	<form class="well form-horizontal" action=" " method="post"  id="contact_form" enctype="multipart/form-data">
-		<?php include_once './forms/admin_users_form.php'; ?>
-	</form>
-</div>
+    <div id="page-wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Add User</h2>
+            </div>
+        </div>
+        <!-- Success message -->
+        <form class="well form-horizontal" action=" " method="post"  id="contact_form" enctype="multipart/form-data">
+            <?php include_once './forms/admin_users_form.php'; ?>
+        </form>
+    </div>
 
 
 
