eb2ec31eab844bdfec5feef0c984fae0588ee222
Fisayo Afolayan <afolayanfisayo@gmail.com>
2020-10-06 16:41:19 +0200

    Code clean up events

--- add_event.php
+++ add_event.php
@@ -1,60 +1,60 @@
 <?php
+
 session_start();
 require_once './config/config.php';
 require_once './includes/auth_validate.php';
 
 
 //serve POST method, After successful insert, redirect to customers.php page.
-if ($_SERVER['REQUEST_METHOD'] == 'POST') 
-{
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     //Mass Insert Data. Keep "name" attribute in html form same as column name in mysql table.
-    $data_to_store = filter_input_array(INPUT_POST);
+    $data = filter_input_array(INPUT_POST);
     //Insert timestamp
-    $data_to_store['created_at'] = date('Y-m-d H:i:s');
-    $db = getDbInstance();
-    $last_id = $db->insert ('customers', $data_to_store);
+    $data['created_at'] = date('Y-m-d H:i:s');
+    $dbInstance = getDbInstance();
     
-    if($last_id)
-    {
-    	$_SESSION['success'] = "Customer added successfully!";
-    	header('location: events.php');
-    	exit();
-    }  
+    $storedDataId = $dbInstance->insert('customers', $data);
+
+    if ($storedDataId) {
+        $_SESSION['success'] = "Customer added successfully!";
+        header('location: events.php');
+        exit();
+    }
 }
 
 //We are using same form for adding and editing. This is a create form so declare $edit = false.
 $edit = false;
 
-require_once 'includes/header.php'; 
+require_once 'includes/header.php';
 ?>
-<div id="page-wrapper">
-<div class="row">
-     <div class="col-lg-12">
-            <h2 class="page-header">Add Event</h2>
+    <div id="page-wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Add Event</h2>
+            </div>
+
         </div>
-        
-</div>
-    <form class="form" action="" method="post"  id="customer_form" enctype="multipart/form-data">
-       <?php  include_once('./forms/event_form.php'); ?>
-    </form>
-</div>
+        <form class="form" action="" method="post"  id="customer_form" enctype="multipart/form-data">
+            <?php  include_once('./forms/event_form.php'); ?>
+        </form>
+    </div>
 
 
-<script type="text/javascript">
-$(document).ready(function(){
-   $("#event_form").validate({
-       rules: {
-            f_name: {
-                required: true,
-                minlength: 3
-            },
-            l_name: {
-                required: true,
-                minlength: 3
-            },   
-        }
-    });
-});
-</script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $("#event_form").validate({
+                rules: {
+                    f_name: {
+                        required: true,
+                        minlength: 3
+                    },
+                    l_name: {
+                        required: true,
+                        minlength: 3
+                    },
+                }
+            });
+        });
+    </script>
 
 <?php include_once 'includes/footer.php'; ?>
